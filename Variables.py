529e826f48fdba7d300edc3dc8d1d93216bfa20b
danyal2075 <danyal2075@gmail.com>
2022-08-02 19:11:56 +0200

    Arithematic

--- Variables.py
+++ Variables.py
@@ -1,2 +1,12 @@
-age = 16
+from numpy import True_
+from sqlalchemy import true
+
+
+adult = True
+age = 27
+var_str = "Hello world"
+if (adult== True):
+    det = "The age of Danyal is 27"
+    print(type(det))
 print(age)
+print(var_str)
