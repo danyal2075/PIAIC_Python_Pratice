a3689f3faf1f84b92a2ded1580d1db49dff788df
danyal2075 <danyal2075@gmail.com>
2022-08-05 01:30:17 +0200

    Updated ari_ope

--- Variables.py
+++ Variables.py
@@ -1,12 +1,12 @@
-from numpy import True_
-from sqlalchemy import true
+# type : it is used to see the data type of the varaibles.
 
 
 adult = True
-age = 27
+age = 27    # Declaration
 var_str = "Hello world"
 if (adult== True):
     det = "The age of Danyal is 27"
+    print(det)
     print(type(det))
 print(age)
 print(var_str)
--- arithematic_operations.py
+++ arithematic_operations.py
@@ -1,1 +1,29 @@
-#
+# Arithematic Operations
+num1 = 10
+num2 = 20
+print("Mutiplication : ",num1 * num2)
+print("Addition : ",num1 + num2)
+print("Subtarction : ",num1 - num2)
+print("Division : ",num1 / num2)
+print(num1 * (10 - (20 + 30)))  # brackets  used for prefernce.
+print ("Reminder : ", 7 % 5)
+
+# Increment: 
+
+print("This is used for increment")
+# We can also take a variable and then increment in the original value
+incre = 1000
+salary = 10000
+
+salary += incre
+#salary += 1000  # it depends on how much you would like to add
+
+salary -= incre
+# This means salary = salary - incre
+
+salary *= incre
+# This means salary = salary * incre
+
+salary /= incre 
+# This means salary = salary / incre
+print("The new salary is : ", salary)
